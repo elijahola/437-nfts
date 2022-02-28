5074f02e8a4af9109b1c3b17e6efc16f1d4507a9
Isaiah Ware <isaiahware99@gmail.com>
2022-02-28 01:15:33 -0600

    added comments and cleaned up files

--- src/App.jsx
+++ src/App.jsx
@@ -4,51 +4,55 @@ import { useMoralis } from "react-moralis";
 // import thirdweb
 import { useWeb3 } from "@3rdweb/hooks";
 
-const App = ({ isServerInfo }) => {
-  const { isWeb3Enabled, enableWeb3, isAuthenticated, isWeb3EnableLoading } =
-    useMoralis();
+const App = () => {
+    // pulls relevant Moralis state objects from Moralis hook
+    const { isWeb3Enabled, enableWeb3, isAuthenticated, isWeb3EnableLoading } =
+        useMoralis();
 
-  useEffect(() => {
-    if (isAuthenticated && !isWeb3Enabled && !isWeb3EnableLoading) enableWeb3();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [isAuthenticated, isWeb3Enabled]);
+    // Makes sure Web3 for Moralis is on
+    useEffect(() => {
+        if (isAuthenticated && !isWeb3Enabled && !isWeb3EnableLoading)
+            enableWeb3();
+    }, [isAuthenticated, isWeb3Enabled]);
 
-  const { web3, Moralis, user } = useMoralis();
+    // Get relevant Moralis state objects from Moralis hook
+    const { web3, Moralis, user } = useMoralis();
 
-  useEffect(() => {
-    if (isAuthenticated && !isWeb3Enabled && !isWeb3EnableLoading) enableWeb3();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [isAuthenticated, isWeb3Enabled]);
+    // Use the connectWallet hook thirdweb gives us.
+    const { connectWallet, address, error, provider } = useWeb3();
+    console.log("👋 Address:", address);
 
-  // Use the connectWallet hook thirdweb gives us.
-  const { connectWallet, address, error, provider } = useWeb3();
-  console.log("👋 Address:", address);
-
-  const options = { chain: "rinkeby", address: address };
+    // Grab all NFTs from a user's wallet and print them to the console
     Moralis.Web3API.account
-    .getNFTs(options)
-    .then((nfts) => console.log(nfts));
+        .getNFTs({
+            chain: "rinkeby",
+            address: address,
+        })
+        .then((polygonNFTs) => console.log(polygonNFTs));
 
-  // This is the case where the user hasn't connected their wallet
-  // to your web app. Let them call connectWallet.
-  if (!address) {
+    // This is the case where the user hasn't connected their wallet
+    // to your web app. Let them call connectWallet.
+    if (!address) {
+        return (
+            <div className="landing">
+                <h1>Welcome to Our NFT Analytics</h1>
+                <button
+                    onClick={() => connectWallet("injected")}
+                    className="btn-hero"
+                >
+                    Connect your wallet
+                </button>
+            </div>
+        );
+    }
+
+    // This is the case where we have the user's address
+    // which means they've connected their wallet to our site!
     return (
-      <div className="landing">
-        <h1>Welcome to Our NFT Analytics</h1>
-        <button onClick={() => connectWallet("injected")} className="btn-hero">
-          Connect your wallet
-        </button>
-      </div>
+        <div className="landing">
+            <h1>👀 wallet connected, now what!</h1>
+        </div>
     );
-  }
-
-  // This is the case where we have the user's address
-  // which means they've connected their wallet to our site!
-  return (
-    <div className="landing">
-      <h1>👀 wallet connected, now what!</h1>
-    </div>
-  );
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -14,26 +14,25 @@ const supportedChainIds = [4];
 // Include what type of wallet you want to support.
 // In this case, we support Metamask which is an "injected wallet".
 const connectors = {
-  injected: {},
+    injected: {},
 };
 
-// Used for
+// Used for connecting to Moralis so we can pull user NFTs. I left these here without using environment variables
+// so you guys can test (not secure). I can generate a new SERVER_URL and APP_ID at any time
 const SERVER_URL = "https://54dz9wfocpu0.usemoralis.com:2053/server";
 const APP_ID = "UMnyq2Mr8Gr4gBqsg8xzfGOXuajilRxFojia5tHM";
 
-const isServerInfo = APP_ID && SERVER_URL ? true : false;
-
-// Finally, wrap App with ThirdwebWeb3Provider.
+// Finally, wrap App with the MoralisProvider and the ThirdwebWeb3Provider
 ReactDOM.render(
-  <React.StrictMode>
-    <MoralisProvider appId={APP_ID} serverUrl={SERVER_URL}>
-        <ThirdwebWeb3Provider
-          connectors={connectors}
-          supportedChainIds={supportedChainIds}
-        >
-          <App isServerInfo />
-        </ThirdwebWeb3Provider>
-    </MoralisProvider>
-  </React.StrictMode>,
-  document.getElementById("root")
+    <React.StrictMode>
+        <MoralisProvider appId={APP_ID} serverUrl={SERVER_URL}>
+            <ThirdwebWeb3Provider
+                connectors={connectors}
+                supportedChainIds={supportedChainIds}
+            >
+                <App />
+            </ThirdwebWeb3Provider>
+        </MoralisProvider>
+    </React.StrictMode>,
+    document.getElementById("root")
 );
