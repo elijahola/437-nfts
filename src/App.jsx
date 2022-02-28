da07aac3134c9269dc6b1d3ad4f4cd9971c17ff5
Isaiah Ware <isaiahware99@gmail.com>
2022-02-27 19:05:38 -0600

    pull nfts using moralis

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,33 @@
-
 import { useEffect, useMemo, useState } from "react";
+import { useMoralis } from "react-moralis";
 
 // import thirdweb
 import { useWeb3 } from "@3rdweb/hooks";
 
-const App = () => {
+const App = ({ isServerInfo }) => {
+  const { isWeb3Enabled, enableWeb3, isAuthenticated, isWeb3EnableLoading } =
+    useMoralis();
+
+  useEffect(() => {
+    if (isAuthenticated && !isWeb3Enabled && !isWeb3EnableLoading) enableWeb3();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isAuthenticated, isWeb3Enabled]);
+
+  const { web3, Moralis, user } = useMoralis();
+
+  useEffect(() => {
+    if (isAuthenticated && !isWeb3Enabled && !isWeb3EnableLoading) enableWeb3();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isAuthenticated, isWeb3Enabled]);
+
   // Use the connectWallet hook thirdweb gives us.
   const { connectWallet, address, error, provider } = useWeb3();
-  console.log("👋 Address:", address)
+  console.log("👋 Address:", address);
+
+  const options = { chain: "rinkeby", address: address };
+    Moralis.Web3API.account
+    .getNFTs(options)
+    .then((nfts) => console.log(nfts));
 
   // This is the case where the user hasn't connected their wallet
   // to your web app. Let them call connectWallet.
@@ -21,13 +41,14 @@ const App = () => {
       </div>
     );
   }
-  
+
   // This is the case where we have the user's address
   // which means they've connected their wallet to our site!
   return (
     <div className="landing">
       <h1>👀 wallet connected, now what!</h1>
-    </div>);
+    </div>
+  );
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { MoralisProvider, useMoralis } from "react-moralis";
 
 // Import ThirdWeb
-import { ThirdwebWeb3Provider } from '@3rdweb/hooks';
+import { ThirdwebWeb3Provider } from "@3rdweb/hooks";
 
 // Include what chains you wanna support.
 // 4 = Rinkeby.
@@ -16,15 +17,23 @@ const connectors = {
   injected: {},
 };
 
+// Used for
+const SERVER_URL = "https://54dz9wfocpu0.usemoralis.com:2053/server";
+const APP_ID = "UMnyq2Mr8Gr4gBqsg8xzfGOXuajilRxFojia5tHM";
+
+const isServerInfo = APP_ID && SERVER_URL ? true : false;
+
 // Finally, wrap App with ThirdwebWeb3Provider.
 ReactDOM.render(
   <React.StrictMode>
-    <ThirdwebWeb3Provider
-      connectors={connectors}
-      supportedChainIds={supportedChainIds}
-    >
-      <App />
-    </ThirdwebWeb3Provider>
+    <MoralisProvider appId={APP_ID} serverUrl={SERVER_URL}>
+        <ThirdwebWeb3Provider
+          connectors={connectors}
+          supportedChainIds={supportedChainIds}
+        >
+          <App isServerInfo />
+        </ThirdwebWeb3Provider>
+    </MoralisProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
