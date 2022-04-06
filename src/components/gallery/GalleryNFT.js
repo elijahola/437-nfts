2a97c4b5b7e084e6a5c5b6f98c27f9c19902320c
elijahola <elijaho@wustl.edu>
2022-04-06 13:06:11 -0400

    gallery

--- src/components/gallery/GalleryNFT.js
+++ src/components/gallery/GalleryNFT.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "../../styles/gallery/Gallery.css";
+import noimage from "../../assets/noimage.png"
+import { getTokenUri } from "@3rdweb/sdk";
 
 const GalleryNFT = (nft) => {
     let metadata = {};
@@ -7,6 +9,7 @@ const GalleryNFT = (nft) => {
     if (nft !== null && nft.nft !== null && nft.nft.metadata !== null) {
         metadata = JSON.parse(nft.nft.metadata);
     }
+    
 
     const name =
         metadata.hasOwnProperty("name") === true
@@ -16,9 +19,21 @@ const GalleryNFT = (nft) => {
         metadata.hasOwnProperty("description") === true
             ? metadata["description"]
             : "No description found!";
-    const image =
-        metadata.hasOwnProperty("image") === true ? metadata["image"] : "";
 
+    
+    
+    
+    let image =
+        metadata.hasOwnProperty("image") === true ? metadata["image"] : noimage;
+    //console.log(image);
+
+    if(image.charAt(0) == "i"){
+        const i = "https://ipfs.io/ipfs/"
+        const newUrl = image.slice(7);
+        const combine = i + newUrl;
+        image = encodeURI(combine);     
+    }
+    
     return (
         <div className="gallery-nft">
             <img src={image} />
